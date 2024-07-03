cf1c80532cdda34b103fe96544fcd9b6b7546813
= <=>
2024-07-03 09:47:40 -0300

    new utils functions
    rand_vector, mod, vector_to_ints, matrix_to_ints, split, string_to_matrix, mkdir_tree

--- utils.cpp
+++ utils.cpp
@@ -3,9 +3,12 @@
 #include <algorithm>
 #include <cassert>
 #include <cmath>
+#include <iostream>
 #include <random>
 #include <stdexcept>
 #include <string>
+#include <sys/stat.h>
+#include <vector>
 
 using namespace std;
 
@@ -17,6 +20,13 @@ int rand(int cap) {
 	return twister() % cap;
 }
 
+vector<int> rand_vector(int n, int cap) {
+    vector<int> v(n);
+    for(int i = 0; i < n; i++)
+        v[i] = rand(cap);
+    return v;
+}
+
 double rand01() {
     return rand_real(generator);
     //return (double) (twister() % twister.max()) / twister.max();
@@ -26,16 +36,58 @@ bool coin_flip(double p) {
     return rand01() < p;
 }
 
-pair<string,string> split(string str, char delim) {
+int mod(int x, int m) {
+    while(x < 0)
+        x += m;
+    return x % m;
+}
+
+vector<int> vector_to_ints(vector<string> v) {
+    vector<int> ans;
+    for(int i = 0; i < (int)v.size(); i++) {
+        ans.push_back(stoi(v[i]));
+    }
+    return ans;
+}
+
+vector<vector<int>> matrix_to_ints(vector<vector<string>> v) {
+    vector<vector<int>> ans;
+    for(int i = 0; i < (int)v.size(); i++) {
+        ans.push_back(vector_to_ints(v[i]));
+    }
+    return ans;
+}
+
+vector<string> split(string str, char delim) {
+    vector<string> ans;
+    int next_char_idx = 0;
     for(int i = 0; i < (int)str.size(); i++) {
         if(str[i] == delim) {
-            string a = str.substr(0, i);
-            string b = str.substr(i + 1, str.size() - i - 1);
-            return make_pair(a, b);
+            ans.push_back(str.substr(next_char_idx, i - next_char_idx));
+            next_char_idx = i + 1;
         }
     }
-    string delim_str(1, delim);
-    throw invalid_argument("No character " + delim_str + " found in string " + str);
+    
+    if(next_char_idx < (int)str.size()) {
+        ans.push_back(str.substr(next_char_idx, str.size() - next_char_idx + 1));
+    }
+    
+    return ans;
+}
+
+vector<vector<string>> string_to_matrix(string str) {
+    vector<vector<string>> ans;
+    int idx = 1; // Ignore first bracket
+    while(idx < (int)str.size() - 1) { // Ignore last bracket
+        if(str[idx] == '[') {
+            size_t closing_bracket = str.find("]", idx + 1);
+            auto line = split(str.substr(idx + 1, closing_bracket - idx - 1), ',');
+            ans.push_back(line);
+            idx = closing_bracket + 1;
+        }
+        else idx++;
+    }
+    return ans;
 }
 
 double logistic(double x) {
@@ -45,6 +97,13 @@ double logistic(double x) {
 double inverse_logistic(double y) {
     assert(y > 0 && y < 1);
     return log(y / (1 - y));
+}
+
+void mkdir_tree(string folder) {
+    for(int i = 0; i <= (int) folder.size(); i++)
+        if(folder[i] == '/')
+            mkdir(folder.substr(0, i).data(), 0777);
+    mkdir(folder.data(), 0777);
 }
 
 void set_seed(int seed) {
--- utils.hpp
+++ utils.hpp
@@ -3,13 +3,20 @@
 
 #include <algorithm>
 #include <string>
+#include <vector>
 
 int rand(int cap);
+std::vector<int> rand_vector(int n, int cap);
 double rand01();
 bool coin_flip(double p);
-std::pair<std::string,std::string> split(std::string str, char delim);
+int mod(int x, int m);
+std::vector<int> vector_to_ints(std::vector<std::string> v);
+std::vector<std::vector<int>> matrix_to_ints(std::vector<std::vector<std::string>> v);
+std::vector<std::string> split(std::string str, char delim);
+std::vector<std::vector<std::string>> string_to_matrix(std::string str);
 double logistic(double x);
 double inverse_logistic(double y);
+void mkdir_tree(std::string folder);
 void set_seed(int seed);
 
 #endif
