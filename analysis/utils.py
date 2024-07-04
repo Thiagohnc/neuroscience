7b4f822cb0d74fabeb8ddf388a02f8d579ea3b7c
Thiagohnc <thiagohnc@hotmail.com>
2024-07-04 13:34:49 -0300

    function format_int to convert 1000 to 1K

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,6 +1,17 @@
 import re
 
 
+def format_int(str_int):
+    answer = ''
+    as_int = int(str_int)
+    while as_int >= 1000:
+        answer += 'K'
+        if as_int % 1000 == 0:
+            as_int //= 1000
+        else:
+            as_int /= 1000
+    return str(as_int) + answer
+
 def format_decimal(str_decimal):
     remove_trailing_zeroes = re.sub(r'([^0])0*$', r'\1', str_decimal)
     remove_point_if_no_decimal = re.sub(r'\.$', '', remove_trailing_zeroes)
