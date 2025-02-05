82d435a82e60753a0b07803f4ae6ec4e6962cc75
Thiagohnc <thiagohnc@hotmail.com>
2025-02-05 17:16:09 -0300

    using eigen3 to calculate pairwise pearson

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 CC = g++
 FLAGS = -Wall -O3 -std=c++17
+EIGEN = /usr/include/eigen3
 
 all: clean main
 
 main: main.cpp graph_generator.o graph.o analysis.o params.o utils.o io.o
-	$(CC) $(FLAGS) main.cpp graph_generator.o graph.o analysis.o params.o utils.o io.o -o main
+	$(CC) $(FLAGS) main.cpp graph_generator.o graph.o analysis.o params.o utils.o io.o -o main -I $(EIGEN)
 
 io.o: io.cpp io.hpp graph.hpp params.hpp utils.hpp
 	$(CC) $(FLAGS) io.cpp -c -o io.o
@@ -16,7 +17,7 @@ graph.o: graph.cpp graph.hpp utils.hpp
 	$(CC) $(FLAGS)  graph.cpp -c -o graph.o
 
 analysis.o: analysis.cpp analysis.hpp utils.hpp
-	$(CC) $(FLAGS) analysis.cpp -c -o analysis.o
+	$(CC) $(FLAGS) analysis.cpp -c -o analysis.o -I $(EIGEN)
 
 params.o: params.cpp params.hpp utils.hpp
 	$(CC) $(FLAGS) params.cpp -c -o params.o
--- analysis.cpp
+++ analysis.cpp
@@ -2,6 +2,7 @@
 #include "params.hpp"
 #include "utils.hpp"
 #include <algorithm>
+#include <Eigen/Dense>
 #include <cmath>
 #include <fstream>
 #include <string>
@@ -9,263 +10,76 @@
 
 using namespace std;
 
-typedef pair<int,int> pi;
-typedef pair<pi,pi> pii;
-
-class Matrix {
-public:
-	vector<vector<double>> m;
-	int n;
-
-	Matrix() {}
-	Matrix(int n) : n(n) { m.resize(n, vector<double>(n)); }
-	Matrix(int n, int v) : n(n) { m.resize(n, vector<double>(n, v)); }
-	~Matrix() {
-		for(int i = 0; i < n; i++) m[i].clear();
-		m.clear();
-	}
-
-	double at(int r, int c) const {
-		return m[r][c];
-	}
-
-	Matrix operator+ (const Matrix& B) const {
-		Matrix C(n);
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				C.set(i, j, at(i, j) + B.at(i, j));
-			}
-		}
-		return C;
-	}
-
-	Matrix& operator+= (const Matrix& B) {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				set(i, j, at(i, j) + B.at(i, j));
-			}
-		}
-		return *this;
-	}
-
-	Matrix operator- (const Matrix& B) const {
-		Matrix C(n);
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				C.set(i, j, at(i, j) - B.at(i, j));
-			}
-		}
-		return C;
-	}
-
-	Matrix& operator-= (const Matrix& B) {
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				set(i, j, at(i, j) - B.at(i, j));
-			}
-		}
-		return *this;
-	}
-
-	Matrix operator* (const Matrix &B) const {
-		Matrix C(n);
-		if(n <= 64) {
-			for(int i = 0; i < n; i++) {
-				for(int j = 0; j < n; j++) {
-					for(int k = 0; k < n; k++) {
-						C.set(i, j, C.at(i, j) + at(i, k) * B.at(k, j));
-					}
-				}
-			}
-		}
-		else {
-			int half = n/2;
-			Matrix A11 = copy_to(0, 0, half, half);
-			Matrix A12 = copy_to(0, half, half, n);
-			Matrix A21 = copy_to(half, 0, n, half);
-			Matrix A22 = copy_to(half, half, n, n);
-
-			Matrix B11 = B.copy_to(0, 0, half, half);
-			Matrix B12 = B.copy_to(0, half, half, n);
-			Matrix B21 = B.copy_to(half, 0, n, half);
-			Matrix B22 = B.copy_to(half, half, n, n);
-
-			/*
-			Matrix P1 = (A11 + A22) * (B11 + B22);
-			Matrix P2 = (A21 + A22) * B11;
-			Matrix P3 = A11 * (B12 - B22);
-			Matrix P4 = A22 * (B21 - B11);
-			Matrix P5 = (A11 + A12) * B22;
-			Matrix P6 = (A21 - A11) * (B11 + B12);
-			Matrix P7 = (A12 - A22) * (B21 + B22);
-
-			C.copy_from(P1 + P4 - P5 + P7, 0, 0);
-			C.copy_from(P3 + P5, 0, half);
-			C.copy_from(P2 + P4, half, 0);
-			C.copy_from(P1 - P2 + P3 + P6, half, half);*/
-
-			Matrix M1 = A11 * B11;
-			Matrix M2 = A12 * B21;
-			Matrix M3 = A21 * (B22 - B11);
-			Matrix M4 = A22 * B22;
-			Matrix M5 = (A21 + A22) * (B21 + B22);
-			Matrix M6 = (A22 - A12) * (B22 - B12);
-			Matrix M7 = (A22 - A11) * B12;
-
-			C.copy_from(M1 + M2, 0, 0);
-			C.copy_from(M5 - M7, 0, half);
-			C.copy_from(M3 + M6, half, 0);
-			C.copy_from(M5 + M6 - M2 - M4, half, half);
-		}
-
-		return C;
-	}
-
-	void set(int i, int j, double val) {
-		m[i][j] = val;
-	}
-
-	Matrix copy_to(int x0, int y0, int x1, int y1) const {
-		Matrix B(x1 - x0);
-		for(int i = 0; i < x1 - x0; i++) {
-			for(int j = 0; j < y1 - y0; j++) {
-				B.set(i, j, at(x0 + i, y0 + j));
-			}
-		}
-		return B;
-	}
-
-	void copy_from(const Matrix& A, int x0, int y0) {
-		for(int i = 0; i < A.n; i++) {
-			for(int j = 0; j < A.n; j++) {
-				set(x0 + i, y0 + j, A.at(i, j));
-			}
-		}
-	}
-};
-
-double pearson(int a, int b, vector<vector<bool>> &spikes, vector<double> &means, vector<double> &dens, vector<double> &delayed_dens) {
-	const static int delay = param_delay();
-	
-	double num = 0;
-	
-	for(int t = 0; t < (int)spikes[a].size() - delay; t++) {
-		num += (spikes[a][t] - means[a]) * (spikes[b][t + delay] - means[b]);
-	}
-	
-	return num / sqrt(dens[a] * delayed_dens[b]);
-}
-
-int next_power_of_two(int x) {
-	int p = 1;
-	while(p < x) p *= 2;
-	return p;
-}
-
 vector<vector<double>> pearson_all_pairs(vector<vector<bool>> &spikes) {
-	const static int delay = param_delay();
+	const int delay = param_delay();
 	
+	progress_bar(0, 4, "Output: Pearson - Alocação de variáveis");
 	vector<double> means(spikes.size());
+	progress_bar(1, 4, "Output: Pearson - Alocação de variáveis");
 	vector<double> dens(spikes.size());
+	progress_bar(2, 4, "Output: Pearson - Alocação de variáveis");
 	vector<double> delayed_dens(spikes.size());
-	vector<vector<double>> corr(spikes.size(), vector<double>(spikes[0].size()));
+	progress_bar(3, 4, "Output: Pearson - Alocação de variáveis");
+	vector<vector<double>> corr(spikes.size(), vector<double>(spikes.size()));
+	progress_bar(4, 4, "Output: Pearson - Alocação de variáveis");
 	
 	int T = spikes[0].size();
 	int N = spikes.size();
 
-	for(int i = 0; i < (int)spikes.size(); i++) {
+	progress_bar(0, N, "Output: Pearson - Pré-processamento");
+	for(int i = 0; i < N; i++) {
 		means[i] = mean(spikes[i]);
 		dens[i] = delayed_dens[i] = 0;
 		for(int t = 0; t < (int)spikes[i].size() - delay; t++) {
 			dens[i] += (spikes[i][t] - means[i]) * (spikes[i][t] - means[i]);
 			delayed_dens[i] += (spikes[i][t + delay] - means[i]) * (spikes[i][t + delay] - means[i]);
 		}
-		progress_bar(i + 1, (int)spikes.size(), "Output: Pearson - Pré-processamento");
+		progress_bar(i + 1, N, "Output: Pearson - Pré-processamento");
 	}
 
-	int blocksize = next_power_of_two(N);
-	double op1 = pow(blocksize, log2(7) - 1);
-	double op2 = pow(blocksize/2, log2(7) - 1) + N * (N - blocksize/2);
-	if(delay == 0) op2 = pow(blocksize/2, log2(7) - 1) + N * (N - blocksize/2)/2; // Half of operations due to symmetry
-	bool fully_calculated_with_strassen = true;
-	if(op2 < op1) {
-		fully_calculated_with_strassen = false;
-		blocksize /= 2;
-	}
-
-	if(delay == 0 && N*N/2 < op1 && N*N/2 < op2) {
-		fully_calculated_with_strassen = false;
-		blocksize = 0;
-	}
-
+	int blocksize = min((size_t) T,  T/(20 * sizeof(double))); // Each matrix will occupy 5% the size of spike_trains matrix of bool
+	Eigen::MatrixXd A(N, blocksize), B(blocksize, N);
 	int blocks = blocksize == 0 ? 0 : (T - delay + blocksize - 1)/blocksize;
-	const int bsizesquare = blocksize * blocksize;
-	Matrix M(blocksize, 0);
+	Eigen::MatrixXd M = Eigen::MatrixXd::Zero(N, N);
+	progress_bar(0, blocks, "Output: Pearson - Cálculo");
 	for(int b = 0; b < blocks; b++) {
-		Matrix A(blocksize), B(blocksize);
-		for(int i = 0, ispk = 0; i < blocksize; i++, ispk++) {
-			for(int j = 0, jspk = b * blocksize; j < blocksize; j++, jspk++) {
-				A.set(i, j, (ispk < N && jspk < T) ? spikes[ispk][jspk] - means[ispk] : 0);
-				B.set(j, i, (ispk < N && jspk + delay < T) ? spikes[ispk][jspk + delay] - means[ispk] : 0);
+		if(blocksize * blocks != T && b == blocks - 1) {
+			int new_blocksize = T % blocksize;
+			A.resize(N, new_blocksize);
+			B.resize(new_blocksize, N);
+			for(int i = 0, ispk = 0; i < N; i++, ispk++) {
+				for(int j = 0, jspk = b * blocksize; j < new_blocksize; j++, jspk++) {
+					A(i,j) = (ispk < N && jspk < T) ? spikes[ispk][jspk] - means[ispk] : 0;
+					B(j,i) = (ispk < N && jspk + delay < T) ? spikes[ispk][jspk + delay] - means[ispk] : 0;
+				}
+			}
+		}
+		else {
+			for(int i = 0, ispk = 0; i < N; i++, ispk++) {
+				for(int j = 0, jspk = b * blocksize; j < blocksize; j++, jspk++) {
+					A(i,j) = spikes[ispk][jspk] - means[ispk];
+					B(j,i) = spikes[ispk][jspk + delay] - means[ispk];
+				}
 			}
 		}
 
 		M += (A * B);
 
-		if(fully_calculated_with_strassen) progress_bar(b + 1, blocks, "Output: Pearson - Cálculo");
-		else progress_bar(((b + 1) * bsizesquare) / blocks, N * N, "Output: Pearson - Cálculo");
-	}
+		progress_bar(b + 1, blocks, "Output: Pearson - Cálculo");
+ 	}
 
-	if(!fully_calculated_with_strassen) {
-		int calculated = bsizesquare;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(delay == 0 && j < i) continue;
-				if(i >= blocksize || j >= blocksize) {
-					corr[i][j] = pearson(i, j, spikes, means, dens, delayed_dens);
-					if(delay == 0 && i != j) {corr[j][i] = corr[i][j]; calculated++;}
-					calculated++;
-				}
-			}
-			progress_bar(calculated, N * N, "Output: Pearson - Cálculo");
-		}
-	}
-
-	const int N_Strassen = min(N, blocksize);
-
-	for(int i = 0; i < N_Strassen; i++) {
-		for(int j = 0; j < N_Strassen; j++) {
-			corr[i][j] = M.at(i, j) / sqrt(dens[i] * delayed_dens[j]);
+	for(int i = 0; i < N; i++) {
+		for(int j = 0; j < N; j++) {
+			corr[i][j] = M(i, j) / sqrt(dens[i] * delayed_dens[j]);
 		}
 		progress_bar(i + 1, N, "Output: Pearson - Finalização");
 	}
 
 	return corr;
-
-	/*
-	for(int i = 0; i < (int)spikes.size(); i++) {
-		means[i] = mean(spikes[i]);
-		dens[i] = delayed_dens[i] = 0;
-		for(int t = 0; t < (int)spikes[i].size() - delay; t++) {
-			dens[i] += (spikes[i][t] - means[i]) * (spikes[i][t] - means[i]);
-			delayed_dens[i] += (spikes[i][t + delay] - means[i]) * (spikes[i][t + delay] - means[i]);
-		}
-		progress_bar(i + 1, (int)spikes.size(), "Output: Pearson - Pré-processamento");
-	}
-	
-	for(int i = 0; i < (int)spikes.size(); i++) {
-		for(int j = 0; j < (int)spikes.size(); j++) {
-			corr[i].push_back(pearson(i, j, spikes, means, dens, delayed_dens));
-		}
-		progress_bar(i + 1, (int)spikes.size(), "Output: Pearson - Cálculo");
-	}*/
-	
-	//return corr;
 }
 
 vector<vector<bool>> read_spike_trains_file(string file_path) {
 	vector<vector<bool>> spikes;
-	int readed = 0;
     ifstream file(file_path);
 	string line;
 	
@@ -273,7 +87,6 @@ vector<vector<bool>> read_spike_trains_file(string file_path) {
     if(file.is_open()) {
         while(getline(file,line)) {
 			spikes.push_back(vector_to_bools(split(line, ' ')));
-			readed += line.size() + 1;
         }
     }
 	progress_bar(1, 1, "Leitura do input (spike trains)");
